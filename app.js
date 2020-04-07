645e94ce63fca2ab6950b54d113d15976582cc85
Thinh Vo <ducthinh2410@gmail.com>
2020-04-07 23:19:08 +0300

    temp

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const express = require('express');
 
 // Constants
 const PORT = 8080;
-const HOST = '0.0.0.0';
 
 // App
 const app = express();
@@ -12,5 +11,5 @@ app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
-app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+app.listen(PORT);
+console.log(`Running on PORT: ${PORT}`);
